37ddc5963b28711899744327c1df70f7285abf0e
Smikhalcv <Smikhalcv@yandex.ru>
2020-11-25 00:31:51 +0300

    pull

--- diplom_django/settings.py
+++ diplom_django/settings.py
@@ -25,7 +25,7 @@ from .secret_key import SECRET_KEY
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1'
